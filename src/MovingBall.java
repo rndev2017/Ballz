f306e6183d0bcac840b87a15bcaeabe508658b51
Mourya Chimpiri <51762864+MrMoves1025@users.noreply.github.com>
2019-06-12 20:28:57 -0400

    Update MovingBall.java

--- src/MovingBall.java
+++ src/MovingBall.java
@@ -1,33 +1,46 @@
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
-public class MovingBall extends Rectangle {
-	private int objDeltaX;
-	private int objDeltaY;
-
-	public MovingBall(int initX, int initY, int width, int height) {
-		super(initX, initY, width, height);
-		objDeltaX = 0;
-		objDeltaY = 0;
-	}
-
-	public void move() {
-		super.translate(objDeltaX, objDeltaY);
-	}
-
-	public int getObjDeltaX() {
-		return objDeltaX;
-	}
-
-	public void setBallDeltaX(int ballDeltaX) {
-		this.objDeltaX = ballDeltaX;
-	}
-
-	public int getObjDeltaY() {
-		return objDeltaY;
-	}
-
-	public void setObjDeltaY(int ballDeltaY) {
-		this.objDeltaY = ballDeltaY;
-	}
+public class MovingBall extends Rectangle
+{
+  private int ballDeltaX;
+  private int ballDeltaY;
+  private static int diameter;
+  public MovingBall(int initX, int initY)
+  {
+    super(initX,initY,diameter,diameter);
+    ballDeltaX = 0; 
+    ballDeltaY=0;
+    diameter = 50;
+  }
+  public void move()
+  {
+    super.translate(ballDeltaX, ballDeltaY);
+  }
+  public int getBallDeltaX()
+  {
+    return ballDeltaX;
+  }
+  public void setBallDeltaX(int ballDeltaX)
+  {
+    this.ballDeltaX = ballDeltaX;
+  }
+  public int getBallDeltaY()
+  {
+    return ballDeltaY;
+  }
+  public void setBallDeltaY(int ballDeltaY)
+  {
+    this.ballDeltaY = ballDeltaY;
+  }
+  public void paintComponent(Graphics g)
+  {
+    g.setColor(Color.WHITE);
+    g.fillOval((int)super.getLocation().getX(),(int)super.getLocation().getY(), diameter, diameter);
+  }
+  public int getDiameter()
+  {
+    return diameter;
+  }
 }
