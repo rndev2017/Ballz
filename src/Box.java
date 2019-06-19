cc8ffe0d34165864cdfbebff7aa3dd51ef402438
Mourya Chimpiri <51762864+MrMoves1025@users.noreply.github.com>
2019-06-19 10:42:09 -0400

    Update Box.java

--- src/Box.java
+++ src/Box.java
@@ -1,72 +1,60 @@
 import java.awt.Color;
-
-import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Rectangle;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random;
-
-import javax.swing.JPanel;
-import javax.swing.Timer;
 
 public class Box extends Rectangle {
-	private int x, y;
-	private int health;
-	public static final int width = 100;
-	public static final int height = 100;
+  private int x, y;
+  private int health;
+  public static final int width = (int)Main.screenSize.width/45;
+  public static final int height = (int)Main.screenSize.width/45;
 
-	public Box(int xPos, int yPos) {
-		super(xPos, yPos, width, height);
-		this.health = Game.getLevel();
-	}
+  public Box(int xPos, int yPos,int healthh) {
+    super(xPos, yPos, width, height);
+    this.health = Game.getLevel()+1;
+  }
 
-	public void paint(Graphics g) {
-		g.setColor(getColor(health));
-		g.fillRect((int) getX(), (int) getY(), width, height);
-	}
+  public void paint(Graphics g) {
+    g.setColor(getColor(health));
+    g.fillRect((int) getX(), (int) getY(), width, height);
+  }
 
-	public Color getColor(int health) {
-		if (health <= 10) {
-			return new Color(255, 204, 0); // produces a light orange color
-		}
+  public Color getColor(int health) {
+    if (health%2==0) {
+      return new Color(255, 204, 0); // produces a light orange color
+    }
 
-		if (health > 10 && health <= 25) {
-			return new Color(0, 255, 51);
-		}
-		
-		if(health > 25 && health <= 40)
-		{
-			return new Color(102, 0, 153);
-		}
-		return null;
-	}
-
-	public void setY(double d) {
-		// TODO Auto-generated method stub
-		this.y = (int) d;
-
-	}
-
-	public static void shiftRow(ArrayList<Box[]> boxes) {
-		for (Box[] row : boxes) {
-			for (Box b : row) {
-				if (b != null) {
-					b.translate(0, width+25);
-				}
-			}
-		}
-	}
-	
-	public int getHealth()
-	{
-		return health;
-	}
-	public boolean newHealth(int startingHealth, int decrement)
+    else if (health%3==0) {
+      return new Color(0, 255, 51);
+    }
+    
+    else if(health%5==0)
+    {
+      return new Color(102, 0, 153);
+    }
+    else
+    {
+      return new Color(30,247,192);
+    }
+  }
+  public static void shiftRow(ArrayList<Box[]> boxes) {
+    for (Box[] row : boxes) {
+      for (Box b : row) {
+        if (b != null) {
+          b.translate(0, width + 25);
+        }
+      }
+    }
+  }
+  public int getHealth()
+  {
+    return health;
+  }
+  public void setHealth(int health)
+  {
+    this.health = health;
+  }
+  public boolean newHealth(int startingHealth, int decrement)
   {
     int newHealth = health-decrement;
     if(newHealth>0)
@@ -78,5 +66,4 @@ public class Box extends Rectangle {
       return false;
     }
   }
-
 }
